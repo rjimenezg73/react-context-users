11981dd057335c40e3de10faf5952f165226d8e8
Roberto Jimenez <robertojimenezg@gmail.com>
2022-11-23 22:44:39 -0600

    Llamadas axios

--- src/context/User/UserState.js
+++ src/context/User/UserState.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from "react";
 import UserReducer from './UserReducer';
 import { UserContext } from './UserContext';
+import axios from 'axios';
 
 
 const UserState = (props) => {
@@ -13,13 +14,15 @@ const UserState = (props) => {
   const [state, dispatch] = useReducer(UserReducer, initialState);
 
   // Ésta función nos va a permitir obtener una lista de usuarios
-  const getUsers = () => {
-
+  const getUsers = async () => {
+    const res = await axios.get('https://reqres.in/api/users');
+    console.log(res);
   }
 
   // Ésta función nos va a permitir obtener un usuario en espécifico
-  const getProfile = () => {
-
+  const getProfile = async(id) => {
+    const res = await axios('https://reqres.in/api/users/' + id);
+    console.log(res);
   }
 
 
